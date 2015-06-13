c98c7cb4b3b088433a6e76cfdb238e06f86d5b95
Brian Wynn <brian.wynn83@gmail.com>
2015-06-13 16:36:56 -0700

    Write a line instead of console message for the event conditional statement. Next, I need to translate the wheel size values so to actually determine/get the wheel diameter from the obscure sizes.

--- construct-this/script.js
+++ construct-this/script.js
@@ -1,57 +1,60 @@
-// create a new constructor function
-function Bike() {};
+(function(){
+  // create a new constructor function
+  function Bike() {};
 
-// arg1 = chainring teeth, arg2 = cassette teeth
-Bike.prototype.range = function(arg1, arg2) {
-  return arg1/arg2;
-};
+  // arg1 = chainring teeth, arg2 = cassette teeth
+  Bike.prototype.range = function(arg1, arg2) {
+    return arg1/arg2;
+  };
 
-Bike.prototype.wheelDiameter = function(arg1) {
-  return arg1;
-};
+  Bike.prototype.wheelDiameter = function(arg1) {
+    return arg1;
+  };
 
-// create a new bike object
-var newBike = new Bike();
+  // create a new bike object
+  var newBike = new Bike();
 
-// equation for gear inches
-newBike.gearInches = function(arg1, arg2) {
-  return (arg1/arg2).toFixed(3);
-};
+  // equation for gear inches
+  newBike.gearInches = function(arg1, arg2) {
+    return (arg1/arg2).toFixed(3);
+  };
 
-newBike.answer = function() {
-  var rings = parseInt(chainring.value);
-  var cassette = parseInt(cogs.value);
-  var answer = document.getElementById("answer");
-  return answer.innerHTML = newBike.gearInches(rings, cassette) + " gear inches.";
-  //console.log(rings);
-  //console.log(cassette);
-  //console.log(newBike.gearInches(parseInt(chainring.value), parseInt(cogs.value)));
-}
+  newBike.answer = function() {
+    var rings = parseInt(chainring.value);
+    var cassette = parseInt(cogs.value);
+    var answer = document.getElementById("answer");
+    return answer.innerHTML = newBike.gearInches(rings, cassette) + " gear inches.";
+    //console.log(rings);
+    //console.log(cassette);
+    //console.log(newBike.gearInches(parseInt(chainring.value), parseInt(cogs.value)));
+  }
 
-// get first input value
-var chnring = document.getElementsByTagName("input")[0];
-// get second input value
-var cogs = document.getElementsByTagName("input")[1];
-// get the submit button to trigger the calculation
-var submit = document.getElementById('gearInch');
-// get the select element
-var whl = document.getElementById('wheelsize');
+  // get first input value
+  var chnring = document.getElementsByTagName("input")[0];
+  // get second input value
+  var cogs = document.getElementsByTagName("input")[1];
+  // get the submit button to trigger the calculation
+  var submit = document.getElementById('gearInch');
+  // get the select element
+  var whl = document.getElementById('wheelsize');
 
-// create an event that looks at the input values from the user
-submit.addEventListener("click", function(e) {
-  // the the user value from the select options
-  var wheel = whl.options[whl.selectedIndex].value;
-  // prevent default
-  e.preventDefault();
+  // create an event that looks at the input values from the user
+  submit.addEventListener("click", function(e) {
+    // the the user value from the select options
+    var wheel = whl.options[whl.selectedIndex].value;
+    // prevent default
+    e.preventDefault();
 
-  // checks to see if the user has input the values necessary to
-  // check their gear range
-  if (chnring.value !== "" &&
-      cogs.value !== "") {
-        newBike.answer();
-            }
-  // if no values entered, let the user know!
-  else {
-        return console.log("enter a value");
-      }
-},false);
+    // checks to see if the user has input the values necessary to
+    // check their gear range
+    if (chnring.value !== "" &&
+        cogs.value !== "") {
+          newBike.answer();
+              }
+    // if no values entered, let the user know!
+    else {
+          var answer = document.getElementById("answer");
+          return answer.innerHTML = "Please fill in missing fields";
+        }
+  },false);
+})();
