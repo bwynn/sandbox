4054e7dfe03d925be3e7ad68cfbd4cae80049255
Brian Wynn <b_wynn@apple.com>
2015-06-15 11:29:09 -0700

    Cleaned up the code, removed commented out constructor functions.

--- construct-this/script.js
+++ construct-this/script.js
@@ -1,6 +1,6 @@
 (function(){
 
-  // Originally used a constructor function to build up this page, however, this page and code is much more suitable for an object literal, which is what's being implemented here now. 
+  // Originally used a constructor function to build up this page, however, this page and code is much more suitable for an object literal, which is what's being implemented here now.
   var newBike = {
     wheelDiameter: function() {
       // get the select element
@@ -27,7 +27,7 @@
       return (arg1 * (arg2/arg3)).toFixed(3);
     },
     answer: function() {
-      var rings = parseInt(chainring.value);
+      var rings = parseInt(chnring.value);
       var cassette = parseInt(cogs.value);
       var wheel = newBike.wheelDiameter();
       var answer = document.getElementById("answer");
@@ -37,50 +37,6 @@
       return answer.innerHTML = newBike.gearInches(wheel, rings, cassette) + " gear inches.";
     }
   };
-  // create a new constructor function
-  /*function Bike() {};
-
-  // create the prototype constructor object for the wheel diameter function
-  Bike.prototype.wheelDiameter = function() {
-    // get the select element
-    var whl = document.getElementById('wheelsize');
-    // the the user value from the select options
-    var wheel = whl.options[whl.selectedIndex].text;
-    // prevent default
-
-    // run conditional to gather user selected option and return the inch
-    // converted value.
-    if (wheel == "26") {
-      return 26;
-    } else if (wheel == "27 1/4") {
-      return 27.25;
-    } else if (wheel == "650b") {
-      return 27.5;
-    } else if (wheel == "700c") {
-      return 29;
-    } else if (wheel == "29er") {
-      return 29;
-    }
-  };*/
-
-  // create a new bike object
-  //var newBike = new Bike();
-
-  // equation for gear inches
-  /*newBike.gearInches = function(arg1, arg2, arg3) {
-    return (arg1 * (arg2/arg3)).toFixed(3);
-  };
-
-  newBike.answer = function() {
-    var rings = parseInt(chainring.value);
-    var cassette = parseInt(cogs.value);
-    var wheel = newBike.wheelDiameter();
-    var answer = document.getElementById("answer");
-    //console.log(rings);
-    //console.log(cassette);
-    //console.log(newBike.gearInches(wheel, rings, cassette));
-    return answer.innerHTML = newBike.gearInches(wheel, rings, cassette) + " gear inches.";
-  }*/
 
   // get first input value
   var chnring = document.getElementsByTagName("input")[0];
@@ -97,7 +53,7 @@
     // check their gear range
     if (chnring.value !== "" &&
         cogs.value !== "") {
-          newBike.answer();
+          return newBike.answer();
               }
     // if no values entered, let the user know!
     else {
