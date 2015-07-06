28d518acbf34176ff9b034231354f8642eda3b9a
Brian Wynn <b_wynn@apple.com>
2015-07-06 16:27:05 -0700

    Okay, working in current state, window loads with the text area in focus.

--- dom-injector/script.js
+++ dom-injector/script.js
@@ -1,4 +1,4 @@
-var hello = function() {
+var msg = function() {
   // create object with element nodes for easy creation
   var p = document.createElement('p');
   // get content container
@@ -7,25 +7,49 @@ var hello = function() {
   var added = content.appendChild(p);
   // get the textarea element
   var userInput = document.getElementById('userInput');
-  // add conditional for text area validation
-  if (userInput.value === '') {
-    added.setAttribute("class", "alert");
-    return added.innerHTML = "Please enter some text";
-  } else {
-    return added.innerHTML = userInput.value;
-  }
+  // return user input value
+  return added.innerHTML = userInput.value;
 };
 
+// anon function takes a function as an argument
+var validator = function(fn) {
+  var elem = document.createElement("p"),
+      container = document.getElementById("content"),
+      content = container.appendChild(elem),
+      alertMsg = document.querySelectorAll('p.alert'),
+      msg = "Please add some text.";
+
+  // conditional determines return value of function argument
+  if(fn() === "") {
+    // set class
+    content.setAttribute("class", "alert");
+    // return message
+    content.innerHTML = msg;
+  } else {
+    return;
+  }
+}
+
+// function to clear out the content of the text area after the user submits
+// their content.
 var clearInput = function() {
   var userInput = document.getElementById('userInput');
   return userInput.value = '';
 };
 
+// event trigger for the text input submission
 (function() {
   var sayHello = document.getElementById('sayHello');
+  var userInput = document.getElementById('userInput');
   sayHello.addEventListener("click", function(e) {
     e.preventDefault();
-    hello();
+    validator(msg);
     clearInput();
+    userInput.focus();
   },false);
 }());
+
+// give focus to the text area when page loads.
+window.onload = function() {
+  document.getElementById("userInput").focus();
+};
