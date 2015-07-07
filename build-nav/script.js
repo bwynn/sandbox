e2b40d312cbf785b58780e6b8bb1af2880102126
Brian Wynn <b_wynn@apple.com>
2015-07-07 16:42:05 -0700

    Did several things here - changed generated content to match semantic requirements, created three different dom generation functions to build up the navigation content based on the collection object's array. First, build the navigation list elements within the buildNavBoxes function, then build the anchor elements based on the urls provided within the buildNavLinks() and finally generate the nav content based on the albums array within the collection object, instantiated as buildNavContent().

--- build-nav/script.js
+++ build-nav/script.js
@@ -1,21 +1,12 @@
-// create an immediate instantiated function, thus protecting the content
-//(function() {
-
-  // constructor function to fiddle around with
-  /*var Collection = function() {};
-
-  Collection.prototype.albums = function(arg) {
-    var result = [];
-    result.push[arg];
-    return result;
-  }*/
-
+// create an immediately instantiated function, thus protecting the content
+(function() {
   // create an object literal with an array to help us determine the array length,
   // in practical application, this will be how we determine the number of
   // navigation list items to create, which can then get the pertinent information
   // to populate
   var collection = {
-    albums: ['one', 'two', 'three', 'seventeen', 'forty two', 'four']
+    albums: ['one', 'two', 'three', 'four', 'five', 'six'],
+    urls: ["#1", "#2", "#3", "#4", "#5", "#6"]
   };
 
   // create an anon function to get length of albums
@@ -24,38 +15,46 @@
   };
 
   // anon function to create container boxes
+  var buildNavBoxes = function() {
     // create for loop using albumLength method for sizing
-      // get placement container
-      // create div container element
-      // assign class attribute to newly created element
-      // propagate creation of boxes
-
-  // function to build the list items -- in this case it's a p element, but
-  // it's demonstrating proof of concept
-  var buildIt = function() {
-    // get number of container objects from above anon function
-    // loop through the container elements
-      // create semantic element for content placement
-      // apply class attributes as necessary
-      // append elements into containers
-      // append content from collection object to set content
     for (var i = 0; i < albumLength(); i++) {
-      // create element
-      var placement = document.getElementById("placement");
-          elem = document.createElement("p");
-
-      // append content into its container
-      placement.appendChild(elem);
-
-      // set the class attribute to allow for styling of elements.
-      elem.setAttribute("class", "navigation-boxes");
-
-      // append content based on the collection.albums' array content.
-      elem.innerHTML = collection.albums[i];
+      // get placement container
+      var placement = document.getElementById("placement"),
+      // create div container element
+          cont = document.createElement("li");
+      // assign class attribute to newly created element
+      cont.setAttribute("class", "navigation-boxes");
+      // propagate creation of boxes
+      placement.appendChild(cont);
     }
-  };
+  }();
 
-  // instantiate the buildIt function
-  buildIt();
+  // function to build the anchor tags based on the number of elements found
+  // within the collection urls array
+  var buildNavLinks = function() {
+    // get number of container objects from above anon function
+    var containers = document.getElementsByClassName("navigation-boxes");
+    // loop through the container elements
+    for (var i = 0; i < containers.length; i++) {
+      // create semantic element for content placement
+      var elem = document.createElement("a");
+      // apply class attributes as necessary
+      elem.setAttribute("class", "nav-content");
+      // append elements into containers
+      containers[i].appendChild(elem);
+      // append content from collection object to set content
+      elem.setAttribute("href", collection.urls[i]);
+    }
+  }();
 
-//}());
+  // function to build the contents of the anchor tags, based on the
+  // collection object's information from the albums array
+  var buildNavContent = function() {
+    // get anchor elements from nav
+    var anchors = document.querySelectorAll("a.nav-content");
+    // loop through so as to assign content
+    for (var i = 0; i < anchors.length; i++) {
+      anchors[i].innerHTML = collection.albums[i];
+    }
+  }();
+}());
