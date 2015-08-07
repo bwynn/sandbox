e0c470c06a95ae235c2dbe9259bf5b95f0f8a29d
Brian Wynn <b_wynn@apple.com>
2015-08-07 13:10:36 -0700

    Modified the patternLogic method to gather the clicked item and push it into a global scope array. Also removes the hide class from the selected element. The conditional on which those assignments are placed qualifies the object by verifying the presences of the class attribute .active.

--- memory-game/js/script.js
+++ memory-game/js/script.js
@@ -8,12 +8,15 @@
     jquery_map: {
       box_wrap: $("#boxWrap"),
       new_box: $('<div class="box"/>'),
-      box: $(".box")
+      box: $("div.box")
     },
     settable_map: {
       cur_index: 4,
       hint_active: true,
       hint_inactive: false
+    },
+    grid_map: {
+      values: []
     }
   };
   // ----------------------- END MODULE SCOPE VARIABLES ------------------------
@@ -55,37 +58,79 @@
         return Math.floor((Math.random() * buildGrid( configMap.settable_map.cur_index )) + 0);
   };
 
+  // Begin DOM method /createPattern/
   // this function determines the squares to assign the
   // active class to as determined by the returned value of the
   // newgrid.pattern() method.
-  var patternLogic = function() {
+  var createPattern = function() {
     // get box
     var boxes = $("div.box"),
+        pat = pattern(),
         // creates active object which is the randomly assigned box
-        active = boxes[pattern()],
-        sibling1, sibling2;
+        active, sibling1, sibling2, newIndex;
 
-    // adds active value to the active box
-    if (active >= boxes[15]) {
-      sibling1 = active.previousSibling;
-      sibling2 = sibling1.previousSibling;
+    // changes index if the pattern variable exceeds 14
+    if (pat >= 14) {
+      pat = pat - 2;
     }
-    else {
-      sibling1 = active.nextSibling;
-      sibling2 = sibling1.nextSibling;
-    }
+    // declare new active index
+    newIndex = boxes[pat];
+    sibling1 = newIndex.nextSibling;
+    sibling2 = sibling1.nextSibling;
 
-    active.classList.add("active");
+    newIndex.classList.add("active");
     sibling1.classList.add("active");
     sibling2.classList.add("active");
     // sets the timeout to hide the active box
     setTimeout(function() {
-      active.classList.add("hide");
+      newIndex.classList.add("hide");
       sibling1.classList.add("hide");
       sibling2.classList.add("hide");
     },2000);
 
+    // return index object
+    return newIndex;
   };
+  // End DOM method /createPattern/
+
+  // Begin DOM method /patternLogic/
+  var patternLogic = function( obj ) {
+
+    if ( obj.hasClass("active") === true ) {
+      // remove the hide class from the obj.classList
+      obj.removeClass("hide");
+      // push value into grid_map.values array
+      configMap.grid_map.values.push(obj);
+      console.log(configMap.grid_map.values);
+    }
+    else {
+      console.log("wrong");
+    }
+    // conditional to validate selected boxes attributes
+    // if the clicked div item has the active class
+    //if ( obj === "div.box.active.hide" ) {
+      //return true;
+      // this shows the user the same color selection that they had
+      // been originally shown
+      //obj.removeClass(".hide");
+      // add the clicked value to the values array
+      //values.push( $(this) );
+      // invoke the winner function
+      //if (values.length === 3) {
+        //alert("you win!");
+        //location.reload();
+      //}
+    //} //else {
+      // visual indication that the user got the wrong answer
+      //$(this).addClass("wrong");
+      // right now alert is present, but this should be
+      // a pop up modal that prevents the user from further interaction
+      //alert("Try Again!");
+      //location.reload();
+    //}
+  };
+  // End DOM method /patternLogic/
+  //
   // ----------------------- END DOM METHODS -----------------------------------
 
   // ----------------------- EVENT HANDLERS ------------------------------------
@@ -99,37 +144,16 @@
   // if the box clicked has the active class.
   var gameplay = function() {
     var boxes = $("div.box");
-    boxes[i].addEventListener("click", function(e) {
-      // declare an array to store the items clicked on
-      var values = [];
-    // conditional to validate selected boxes attributes
-      if ($(this).attr('class').nodeValue == "box active hide") {
-        // this shows the user the same color selection that they had
-        // been originally shown
-        $(this).remove(".hide");
-        // add the clicked value to the values array
-        values.push( $(this) );
-        // invoke the winner function
-        if (values.length === 3) {
-          alert("you win!");
-          location.reload();
-        }
-      } else {
-        // visual indication that the user got the wrong answer
-        $(this).addClass("wrong");
-        // right now alert is present, but this should be
-        // a pop up modal that prevents the user from further interaction
-        alert("Try Again!");
-        location.reload();
-      }
-    },false);
+    boxes.on("click", function() {
+      patternLogic( $(this) );
+    });
   };
 
 
   var initModule = function() {
     buildBox();
-    patternLogic();
-    //gameplay();
+    createPattern();
+    gameplay();
   };
   initModule();
   //return { initModule : initModule };
