dec10810841ccd184b34a52cb247750a5340675c
Brian Wynn <brian.wynn83@gmail.com>
2015-10-06 00:04:08 -0700

    moving a few things around.

--- units/script.js
+++ units/script.js
@@ -1,16 +1,19 @@
-var userInput = document.getElementById("user-input");
-var outputResult = document.getElementById("result");
-
 // Create a new constructor object. Takes input value directly from
 // text input element, and parses the value into a number, and returns a
 // calculation based on the users settings
-function Unit( input ) {
+function FromInch( input ) {
   this.input = input;
   this.default = function() {
     return parseInt( this.input * 1 );
   };
-  this.toInch = function() {
-    return parseInt( this.input * 0.0393701 );
+  this.toFoot = function() {
+    return parseInt( this.input / 12 );
+  };
+  this.toYard = function() {
+    return parseInt( this.input / 36 );
+  };
+  this.toMile = function() {
+    return parseInt( this.input / 63360 );
   };
   this.toMM = function() {
     return parseInt( this.input * 25.4 );
@@ -21,51 +24,7 @@ function Unit( input ) {
   this.toMeter = function() {
     return parseInt( this.input * 0.254 );
   };
-}
-
-function calc( unit1, unit2 ) {
-  var newValue = new Unit( userInput.value ), // current input value
-      fullNum; // declare fullNum variable for later use.
-
-  // if english is selected
-  if ( unit1.value === "Inch" ) {
-    // and english is output selection
-    if ( unit2.value === "Inch" ) {
-      return newValue.default();
-    }
-    // and metric is output selection
-    if ( unit2.value === "Millimeters" ) {
-			fullNum = newValue.toMM(); // return calculation
-      return fullNum.toFixed(3);
-    }
-    else if ( unit2.value === "Centimeters" ) {
-      fullNum = newValue.toCM();
-      return fullNum.toFixed(3);
-    }
-    else if ( unit2.value === "Meters" ) {
-      fullNum = newValue.toMeters();
-      return fullNum.toFixed(3);
-    }
-  }
-
-  // if metric is selected
-  if ( unit1.value === "Millimeters" ) {
-    // and metric is output selection
-    if ( unit2.value === "Inch" ) {
-      fullNum = newValue.toInch(); // return calculation
-      return fullNum.toFixed(3);
-    }
-    // and english is output selection
-    if ( unit2.value === "Millimeters" ) {
-      return newValue.default();// return calculation
-    }
+  this.toKilometer = function() {
+    return parseInt( this.input * 0.00254 );
   }
 }
-
-
-// input - output event handling
-userInput.addEventListener("keyup", function() {
-  var select = document.getElementById("input-value");
-  var output = document.getElementById("output-value");
-  outputResult.innerHTML = calc( select, output );
-});
