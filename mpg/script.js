82d8eacb1695036ca92d1df6747ecc319b917ae9
Brian Wynn <b_wynn@apple.com>
2015-11-04 14:42:18 -0800

    Created a callback function and prototypal function to get values for a user session to and from localStorage.

--- mpg/script.js
+++ mpg/script.js
@@ -8,29 +8,28 @@
 // LOCAL STORAGE HANDLING
 var date = new Date();
 var today = date.toDateString();
-//console.log(today);
-
-var anotherDate = new Date(2015, 10, 3);
-var yesterday = anotherDate.toDateString();
-
-
-//console.log( yesterday );
 
 // locally declare an array to add values to
 var dates = [];
 // push the values into the array -- 2 arrays, 1 for mpg, 1 for date
-dates.push(today);
-dates.push(yesterday);
-
-// push the values into the local storage values
-localStorage["dates"] = JSON.stringify(dates);
-
-// declare a variable to parse out the local storage properties
-var storedDates = JSON.parse(localStorage.dates);
-
-console.log( storedDates );
 
 // iterate through the values to assign/ appropriate into tables
-for (var i = 0; i < storedDates.length; i++) {
+/*for (var i = 0; i < storedDates.length; i++) {
   console.log( storedDates[i] );
+}*/
+
+// callback function that processes the input values as provided by user
+// into local storage
+function setLocalStorage( arr, prop, val ) {
+  arr.push( val ); // pushing value to outside of functional scope will allow for adding new entries into localStorage string
+  localStorage[prop] = JSON.stringify( arr ); // push the values into the local storage values
 }
+
+var User = function() {};
+User.prototype.dates = function() {
+  // declare a variable to parse out the local storage properties
+  var data = JSON.parse( localStorage.dates );
+  return { data : data };
+}
+
+var user = new User();
