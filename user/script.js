7af9d7a98b2939e071c6235edfd925a7847a58b2
Brian Wynn <b_wynn@apple.com>
2015-11-09 16:58:46 -0800

    Building object instead of push values to arrays using this directory as a mock build while I am working on the mpg project.

--- user/script.js
+++ user/script.js
@@ -1,12 +1,22 @@
 var button = document.querySelector(".main button");
 
-var person = function() {
+/*var person = {
   this.name = localStorage.name;
   this.location = localStorage.location;
   this.age = localStorage.age;
+};*/
+
+var history = [];
+
+var Person = function(name, location, age) {
+  this.name = name;
+  this.location = location;
+  this.age = age;
 };
 
-var newUser = function() {
+var person = new Person();
+
+var newUser = function( obj ) {
   var nameInput = document.getElementById("new-value");
   var name = nameInput.value;
 
@@ -16,16 +26,29 @@ var newUser = function() {
   var ageInput = document.getElementById("new-age");
   var age = ageInput.value;
 
-  localStorage.name = name;
-  localStorage.location = location;
-  localStorage.age = age;
+  obj.name = name;
+  obj.location = location;
+  obj.age = age;
 };
 
+function setData() {
+  history.push( person );
+}
 
-button.addEventListener("click", function(e) {
+function setLocalStorage() {
+  localStorage.history = JSON.stringify( history );
+}
+
+document.getElementById("submit").addEventListener("click", function(e) {
+  e.preventDefault();
+  newUser( person );
+}, false);
+
+
+/*button.addEventListener("click", function(e) {
   newUser();
 });
 
 var userName = localStorage.name;
 var userLocation = localStorage.location;
-var userAge = localStorage.age;
+var userAge = localStorage.age;*/
