ed130f0a4f339443417251d532244883f2d4b926
Brian Wynn <b_wynn@apple.com>
2016-01-04 16:48:41 -0800

    Created an events data file, and am passing it into the template. However, I need to iterate over the event name and add it to a new array for each competitor within the controller to allow for template iteration of events along with a competitors results.

--- jsonApp/public/js/app.js
+++ jsonApp/public/js/app.js
@@ -10,8 +10,8 @@ angular.module("myApp", ['ngRoute'])
       });
   }).
   controller("myCtrl", function($scope, $http) {
-    $scope.world = "World";
 
+    // get the user information
     $http.get('/static/mock/info.json')
       .success(function(response) {
         $scope.competitors = response;
@@ -19,5 +19,18 @@ angular.module("myApp", ['ngRoute'])
       error(function(response, status) {
         console.log(response);
         console.log(status);
-      })
+      });
+
+    // get the event information
+    $http.get('/static/mock/schedule.json').
+      success(function(response) {
+        $scope.events = response;
+      }).
+      error(function(response, status) {
+        console.log(response);
+        console.log(status);
+      });
+
+    // goal is to display each event under a competitor with their respective result in that
+    // particular event. 
   });
